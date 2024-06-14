c5b60e822cd85a7a3c78c2bdfd96864d6ff6ca4e
danyllo-aniceto <danyllo.aniceto.carvalho@gmail.com>
2024-06-14 16:03:34 -0300

    add characeter json

--- src/components/GuessedCountCard/guessedCountCard.tsx
+++ src/components/GuessedCountCard/guessedCountCard.tsx
@@ -6,12 +6,22 @@ interface GuessedCountCardProps {
 
 export function GuessedCountCard({ count }: GuessedCountCardProps) {
   const getColorByCount = (count: number): string => {
-    if (count <= 5) {
+    if (count <= 25) {
       return "#1E90FF";
-    } else if (count <= 10) {
+    } else if (count <= 50) {
       return "#32CD32";
-    } else {
+    } else if (count <= 75) {
       return "#8A2BE2";
+    } else if (count <= 100) {
+      return "#FFA500";
+    } else if (count <= 150) {
+      return "#FF1493";
+    } else if (count <= 200) {
+      return "#FF0000";
+    } else if (count < 250) {
+      return "#00FA9A";
+    } else {
+      return "#FF4500";
     }
   };
   const countColor = getColorByCount(count);
